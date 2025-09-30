3c6fda117848fa9079676a207e6fe21cd26a1d94
Polina Zherikova bc2025-3 <polinagerikova@gmail.com>
2025-09-30 18:33:51 +0300

    Add options -f and -p

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 program
   .option('-i, --input <file>', 'Input JSON file')
   .option('-o, --output <file>', 'Output file')
-  .option('-d, --display', 'Display result in console');
+  .option('-d, --display', 'Display result in console')
+  .option('-f, --furnished', 'Show only furnished houses')
+  .option('-p, --price <number>', 'Show houses with price less than given', Number);
 
 program.parse();
 
@@ -17,16 +19,27 @@ if (!options.input) {
 
 let data;
 try {
-  data = fs.readFileSync(options.input, 'utf8');
+  const raw = fs.readFileSync(options.input, 'utf8');
+  data = JSON.parse(raw); 
 } catch (err) {
   console.error("Cannot find input file");
   process.exit(1);
 }
 
+if (options.furnished) {
+  data = data.filter(h => h.furnishingstatus === 'furnished');
+}
+
+if (options.price !== undefined) {
+  data = data.filter(h => Number(h.price) < options.price);
+}
+
+const outputText = data.map(h => `${h.price} ${h.area}`).join('\n');
+
 if (options.display) {
-  console.log(data);
+  console.log(outputText);
 }
 
 if (options.output) {
-  fs.writeFileSync(options.output, data, 'utf8');
+  fs.writeFileSync(options.output, outputText, 'utf8');
 }
